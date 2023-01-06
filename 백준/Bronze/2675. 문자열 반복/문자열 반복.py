ecb3b2dea72348a632646f7b97d63ed667cab356
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2023-01-07 01:49:36 +0900

    [Bronze II] Title: 문자열 반복, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,7 @@
+a=int(input())
+for i in range(a):
+    num,cha=input().split()
+    for charss in cha:
+            for j in range(int(num)):
+                print(charss,end= '')
+    print()    
