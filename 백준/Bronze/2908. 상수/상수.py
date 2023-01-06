0828a6654dcc1911e359388028085a1ae47acdc9
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2023-01-07 05:17:26 +0900

    [Bronze II] Title: 상수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.py
@@ -0,0 +1,11 @@
+a,b=input().split()
+list=[]
+c=int(a[::-1])
+d=int(b[::-1])
+# for i in range(1,len(str(a))+1):
+    
+#     print(a[len(str(a))-i], end='')
+if c>d:
+    print(c)
+else:
+    print(d)
