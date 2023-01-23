a5ab7b171977d140df52980ffdf6dcb8802a30f7
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2023-01-23 22:51:50 +0900

    [Silver V] Title: 숫자 카드, Time: 724 ms, Memory: 118080 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -0,0 +1,11 @@
+import sys
+n=int(sys.stdin.readline())
+n_list=set(map(int,sys.stdin.readline().split()))
+m=int(sys.stdin.readline())
+m_list=list(map(int,sys.stdin.readline().split()))
+
+for i in m_list:
+    if i in n_list:
+        print(1,end=' ')
+    else:
+        print(0,end=' ')
