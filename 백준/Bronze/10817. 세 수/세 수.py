5acd39ff39c1427a43d0ba69114fce02f1840895
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2023-01-27 13:54:59 +0900

    [Bronze III] Title: 세 수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.py
+++ 백준/Bronze/10817. 세 수/세 수.py
@@ -1,10 +1,2 @@
-from heapq import heappush
-
-from heapq import nlargest
-heap1=[]
-a,b,c=map(int,input().split())
-
-heappush(heap1,a)
-heappush(heap1,b)
-heappush(heap1,c)
-print(nlargest(2,heap1)[-1]) #n번째 큰 숫자까지 리스트로 반환 후 리스트[-1]출력
+a=list(map(int,input().split()))
+print(sorted(a)[1])
