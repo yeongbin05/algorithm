1b34cfc9339754b0f1c6919629100dd7d7a9e764
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2023-01-27 15:21:32 +0900

    [Silver V] Title: 단어 정렬, Time: 72 ms, Memory: 33704 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.py
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -1,7 +1,8 @@
-n=int(input())
+import sys
+n=int(sys.stdin.readline())
 dic={}
 for i in range(n):
-    a=input()
+    a=sys.stdin.readline().rstrip()
     if a in dic:
         pass
     else:
@@ -9,4 +10,4 @@ for i in range(n):
 sorted_dic=sorted(dic)
 
 sorsorted_dic=sorted(sorted_dic,key=len)
-print(*sorsorted_dic)
+print(*sorsorted_dic,sep='\n')
