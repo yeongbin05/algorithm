285b1648361670dcce414dc8f23d88c1bdc9dafd
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2023-02-01 17:39:30 +0900

    [Silver III] Title: 문자열 집합, Time: 3688 ms, Memory: 41944 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,14 +1,16 @@
-n,m=map(int,input().split())
+import sys
+n,m=map(int,sys.stdin.readline().split())
 n_arr=[]
 m_arr=[]
 
 for i in range(n):
-    n_arr.append(input())
+    n_arr.append(sys.stdin.readline())
 for i in range(m):
-    m_arr.append(input())
+    m_arr.append(sys.stdin.readline())
 cnt=0
 for i in m_arr:
     if i in n_arr:
         cnt+=1
+        
 
 print(cnt)
