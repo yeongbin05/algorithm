3fdc209e6a034ce9f902852701786efe02befa77
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2023-02-07 01:25:41 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,23 +1,32 @@
-a=int(input())
-b=int(input())
-arr=[[] for i in range(a+1)]
-visited=[False]*(a+1)
-for i in range(b):
-    x,y=map(int,input().split())
-    arr[x].append(y)
-    arr[y].append(x)
+#컴퓨터 개수
+n=int(input())
+# 연결된 쌍 개수
+m=int(input())
+arr=[[] for i in range(n+1)]
+# 정점 방문했는지 체크
+visited=[False for i in range(n+1)]
+
+
+#연결된 쌍 입력받음
+for i in range(m):
+    #a와 b연결됨
+    a,b=map(int,input().split())
+    arr[a].append(b)
+    arr[b].append(a)
 
 def dfs(start):
+    #마지막 정점 체크
     stack=[start]
     visited[start]=True
-
+    #stack이 빌 때까지
     while stack:
+        #현재 위치
         current=stack.pop()
-        
+
         for adj in arr[current]:
             if visited[adj]==False:
                 visited[adj]=True
-                stack. append(adj)
+                stack.append(adj)
+
 dfs(1)
 print(visited.count(True)-1)
-
