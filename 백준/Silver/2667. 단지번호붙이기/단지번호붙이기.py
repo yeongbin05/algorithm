106af0fd4909434f77b5cc2116e910d72142103a
yeongbin05 <79687739+yeongbin05@users.noreply.github.com>
2023-02-12 08:12:35 +0900

    [Silver I] Title: 단지번호붙이기, Time: 64 ms, Memory: 34160 KB -BaekjoonHub

---
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -0,0 +1,34 @@
+from collections import deque
+def bfs(si,sj):
+    q=deque()
+    q.append((si,sj))
+    visited[si][sj]=1
+    cnt=1
+    while q:
+        ci,cj=q.popleft()
+        for di,dj in ((0,1),(0,-1),(1,0),(-1,0)):
+            ni,nj=ci+di,cj+dj
+
+            if 0<=ni<n and 0<=nj<n and visited[ni][nj]==0 and arr[ni][nj]==1:
+                q.append((ni,nj))
+                visited[ni][nj]=1
+                cnt+=1
+
+    return cnt
+
+
+
+
+n=int(input())
+arr=[list(map(int,input())) for _ in range(n)]
+visited=[[0]*n for _ in range(n)]
+ans=[]
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j]==1 and visited[i][j]==0:
+            ans.append(bfs(i,j))
+
+ans.sort()
+
+print(len(ans), *ans , sep='\n')
